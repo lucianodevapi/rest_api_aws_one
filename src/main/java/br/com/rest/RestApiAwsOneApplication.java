5724af6362f2b4a49db055773defcabdc0b91d9e
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2021-10-07 20:40:37 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/br/com/rest/RestApiAwsOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAwsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAwsOneApplication.class, args);
+	}
+
+}
